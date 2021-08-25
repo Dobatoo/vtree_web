2bccbfe66d440829ee47e28fd6039c376997cd81
Dobatoo <t_kimura_pvt_001214@yahoo.co.jp>
2021-08-25 17:31:35 +0900

    made view and template. main goal has been achieved.

--- mysite/vtrees/urls.py
+++ mysite/vtrees/urls.py
@@ -7,5 +7,7 @@ app_name = 'vtrees'
 urlpatterns = [
     path('', views.index, name='index'),
     path('video/<str:videos_id>/', views.video, name='video'),
-    path('count_register/<str:videos_id>/', views.count_register, name='count_register')
+    path('channel/<str:channels_id>/', views.channel, name='channel'),
+    path('channel/<str:channels_id>/posts', views.channel_posts, name='channel_posts')
+    #path('count_register/<str:videos_id>/', views.count_register, name='count_register')
 ]
--- mysite/vtrees/views.py
+++ mysite/vtrees/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 #from django.template import context, loader
 from django.urls import reverse
-from .models import Videos_info, Videos_detail
+from .models import Channels_detail, Channels_info, Posts, Videos_info, Videos_detail, Children
 
 # Create your views here.
 def index(request):
-    latest_videos_list = Videos_info.objects.order_by('-videos_published')[:5]
+    channels_list = Channels_info.objects.all()
     #template = loader.get_template('vtrees/index.html')
     context = {
-        'latest_videos_list': latest_videos_list,
+        'channels_list': channels_list,
     }
     return render(request, 'vtrees/index.html', context)
     #return HttpResponse(template.render(context, request))
@@ -18,12 +18,38 @@ def index(request):
 
 def video(request, videos_id):
     video_info = get_object_or_404(Videos_info, pk=videos_id)
-    video_detail = get_object_or_404(Videos_detail, pk=videos_id)
+    video_detail = get_object_or_404(Videos_detail, pk=video_info)
+    video_sited = Children.objects.filter(children_video_parent = videos_id)
+    context = {
+        'video_info':video_info,
+        'video_detail':video_detail,
+        'video_sited':video_sited
+        }
     #try:
         #video = Videos.objects.get(pk=videos_id)
     #except Videos.DoesNotExist:
         #raise Http404("Video does not exist")
-    return render(request, 'vtrees/video.html', {'video_info':video_info,'video_detail':video_detail})
+    return render(request, 'vtrees/video.html', context)
+
+def channel(request, channels_id):
+    channel_info = get_object_or_404(Channels_info, pk=channels_id)
+    channel_detail = get_object_or_404(Channels_detail, pk=channel_info)
+    posts = Posts.objects.filter(posts_channel_id = channels_id).order_by('-posts_video_id__videos_published')[:5]
+    context =  {
+        'channel_info':channel_info,
+        'channel_detail':channel_detail,
+        'posts':posts
+        }
+    return render(request, 'vtrees/channel.html', context)
+
+def channel_posts(request, channels_id):
+    channel_info = get_object_or_404(Channels_info, pk=channels_id)
+    posts = Posts.objects.filter(posts_channel_id = channels_id).order_by('-posts_video_id__videos_published').all()
+    context =  {
+        'channel_info':channel_info,
+        'posts':posts
+        }
+    return render(request, 'vtrees/channel_posts.html', context)
 
 def count_register(request, videos_id):
     video_detail = get_object_or_404(Videos_detail, pk=videos_id)
